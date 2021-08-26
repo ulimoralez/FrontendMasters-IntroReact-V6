f05c696a726939f61402562af4b4ff19bc4e8217
Ulises Moralez <moralezulises@gmail.com>
2021-08-25 22:19:15 -0300

    Prettier was ehre

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,22 +1,33 @@
-const Pet = (props) =>{
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h3", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-    ])
-}
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
-const App = () =>{
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1",{},"Adopt Me!"),
-            React.createElement(Pet, {name: "Luna", animal: "Dog", breed:"Havanese"}),
-            React.createElement(Pet, {name: "Pepper", animal: "Bird", breed:"Cockatiel"}),
-            React.createElement(Pet, {name: "Sudo", animal: "Dog", breed:"Wheaten Terrier"}),
-        ]
-    );
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
 };
 // Dentro de createElement, el primer parámetro es qué vamos a hacer render, el segundo es DÓNDE, en este caso en la etiqueta root
-ReactDOM.render(React.createElement(App, {}, null), document.getElementById('root'));
+ReactDOM.render(
+  React.createElement(App, {}, null),
+  document.getElementById("root")
+);
